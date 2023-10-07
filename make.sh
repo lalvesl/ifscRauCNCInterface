5101a20dc08042235a509a952ba0e2f4f68d34ff
lalvesl <alvesdelima.lucas45@gmail.com>
2023-10-07 11:39:27 -0300

    feat - Cross compiling para sistemas arm7.

---
+++ make.sh
@@ -0,0 +1,37 @@
+rootDocker="docker"
+location="$rootDocker/build"
+dockerImgs="ifscraucncinterface"
+
+listArchs() {
+    echo $(ls -lha $rootDocker | awk '{print $9}' | grep dockerfile)
+}
+
+case $1 in
+init)
+    for arch in $(listArchs); do
+        cat "$rootDocker/$arch"
+        docker build . -t "$dockerImgs/$arch" -f "$rootDocker/$arch"
+        docker run --rm -ti -v $location/app "$dockerImgs/$arch"
+    done
+    ;;
+buildFrontEnd)
+    trunk build
+    ;;
+buildArchs)
+    rm -rf $location
+    mkdir -p $location
+    cp -r dist $location
+    cp -r src-tauri $location
+    cp -r .cargo $location
+    cp -r public $location
+    cp -r src $location
+    cp Cargo* $location
+    for arch in $(listArchs); do
+        echo $arch
+        docker run --rm -ti -v ./:/app "$dockerImgs/$arch"
+    done
+    ;;
+*)
+    echo wrong option
+    ;;
+esac
---
+++ makefile
@@ -0,0 +1,2 @@
+init:
+	make.sh init
