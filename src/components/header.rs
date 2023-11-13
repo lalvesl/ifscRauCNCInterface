e04ca9b6e61f3cede34789f2775ffd001b650a4b
Lucas Alves <alvesdelima.lucas45@gmail.com>
2023-11-13 08:46:39 -0300

    feat - regriado componente Header;

---
+++ src/components/header.rs
@@ -0,0 +1,10 @@
+use leptos::*;
+
+#[component]
+pub fn Header() -> impl IntoView {
+    view! {
+        <header>
+            <img src="public/jaragua_rau_vertical_marca2015_RGB.png" class="logo tauri" alt="IFSC logo"/>
+        </header>
+    }
+}
