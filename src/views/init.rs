1e97e20b5a3ac893f914735ce3728f1bd9b58847
Lucas Alves <alvesdelima.lucas45@gmail.com>
2023-11-24 09:08:49 -0300

    feat - Recriado elementos da tela inicial;

--- src/views/init.rs
+++ src/views/init.rs
@@ -5,10 +5,13 @@ use crate::components::header::Header;
 #[component]
 pub fn InitView() -> impl IntoView {
     view! {
+        <div id="imgbgd"></div>
+        <div id="imgbgdblur"></div>
         <Header/>
         <main class="init-main">
-            <h1>IFSC-Rau CNC PCB Router</h1>
+            <h1>Fresadora CNC para PCIs</h1>
             <h2>Insira um pendrive</h2>
         </main>
+        <h3 id="edital">Edital nº 02/2023/PROPPI - Universal</h3>
     }
 }
