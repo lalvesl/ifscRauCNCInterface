e88d713e398ec74beb7a92f7388e22650bc33a57
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2024-03-15 02:55:43 -0300

    Adicionado Router ao projeto

--- src/app.rs
+++ src/app.rs
@@ -2,13 +2,12 @@ use std::usize;
 
 use leptos::leptos_dom::ev::SubmitEvent;
 use leptos::*;
+use leptos_router::*;
 use serde::{Deserialize, Serialize};
-use serde_wasm_bindgen::to_value;
 use wasm_bindgen::{
     convert::{FromWasmAbi, ReturnWasmAbi},
     prelude::*,
 };
-use web_sys::console;
 
 use crate::{components::header::Header, utils::js_value::*, views::init::InitView};
 
@@ -24,81 +23,22 @@ extern "C" {
     async fn invoke(cmd: &str) -> JsValue;
 }
 
-
-
 #[component]
 pub fn App() -> impl IntoView {
-    // let (name, set_name) = create_signal(String::new());
-    // let (greet_msg, set_greet_msg) = create_signal(String::new());
-
-    // let update_name = move |ev| {
-    //     let v = event_target_value(&ev);
-    //     set_name.set(v);
-    // };
-
-    // let greet = move |ev: SubmitEvent| {
-    //     ev.prevent_default();
-    //     spawn_local(async move {
-    //         if name.get().is_empty() {
-    //             return;
-    //         }
-
-    spawn_local(async move {
-        let new_msg = invoke("list_docs").await;
-
-        // console::log_1(&new_msg);
-        jsvalue_2_vec_str(new_msg).iter().for_each(|d| consoller(d));
-
-        // Array
-        // match to_value(&GreetArgs {
-        //     name: &new_msg.to_string(),
-        // }) {
-        //     Ok(r) => console::log_1(&r),
-        //     Err(_) => (),
-        // }
-
-        // println!("{}", new_msg.is_array());
-    });
-    //         let args = to_value(&GreetArgs { name: &name.get() }).unwrap();
-    //         // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    //         let new_msg = invoke("greet", args).await.as_string().unwrap();
-    //         set_greet_msg.set(new_msg);
-    //     });
-    // };
+    // spawn_local(async move {
+    //     let new_msg = invoke("list_docs").await;
+    //     jsvalue_2_vec_str(new_msg).iter().for_each(|d| consoller(d));
+    // });
 
     view! {
-        <InitView/>
-        // <main class="container">
-        //     <div class="row">
-        //         <a href="https://tauri.app" target="_blank">
-        //             <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
-        //         </a>
-        //         <a href="https://docs.rs/leptos/" target="_blank">
-        //             <img src="public/leptos.svg" class="logo leptos" alt="Leptos logo"/>
-        //         </a>
-        //     </div>
-
-        //     <p>"Click on the Tauri and Leptos logos to learn more."</p>
-
-        //     <p>
-        //         "Recommended IDE setup: "
-        //         <a href="https://code.visualstudio.com/" target="_blank">"VS Code"</a>
-        //         " + "
-        //         <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank">"Tauri"</a>
-        //         " + "
-        //         <a href="https://github.com/rust-lang/rust-analyzer" target="_blank">"rust-analyzer"</a>
-        //     </p>
-
-        //     <form class="row" on:submit=greet>
-        //         <input
-        //             id="greet-input"
-        //             placeholder="Enter a name..."
-        //             on:input=update_name
-        //         />
-        //         <button type="submit">"Greet"</button>
-        //     </form>
-
-        //     <p><b>{ move || greet_msg.get() }</b></p>
-        // </main>
+        <div id="imgbgd"></div>
+        <div id="imgbgdblur"></div>
+        <Header/>
+        <Router>
+            <Routes>
+                <Route path="" view=InitView/>
+                <Route path="/*any" view=|| view! { <h1>"Not Found"</h1> }/>
+            </Routes>
+        </Router>
     }
 }
--- src/views/init.rs
+++ src/views/init.rs
@@ -1,13 +1,8 @@
 use leptos::*;
 
-use crate::components::header::Header;
-
 #[component]
 pub fn InitView() -> impl IntoView {
     view! {
-        <div id="imgbgd"></div>
-        <div id="imgbgdblur"></div>
-        <Header/>
         <main class="init-main">
             <h1>Fresadora CNC para PCIs</h1>
             <h2>Insira um pendrive</h2>
