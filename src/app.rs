79a3c4c60ebfd03ef2a27ae21e811421d9b4e887
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2024-03-18 11:36:00 -0300

    Adicionado rota about

--- src/app.rs
+++ src/app.rs
@@ -9,34 +9,18 @@ use wasm_bindgen::{
     prelude::*,
 };
 
-use crate::{components::header::Header, utils::js_value::*, views::init::InitView};
-
-// #[wasm_bindgen]
-// extern "C" {
-//     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
-//     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
-// }
-
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
-    async fn invoke(cmd: &str) -> JsValue;
-}
+use crate::{components::header::Header, utils::js_value::*, views::{about::About, init::InitView}};
 
 #[component]
 pub fn App() -> impl IntoView {
-    // spawn_local(async move {
-    //     let new_msg = invoke("list_docs").await;
-    //     jsvalue_2_vec_str(new_msg).iter().for_each(|d| consoller(d));
-    // });
-
     view! {
         <div id="imgbgd"></div>
         <div id="imgbgdblur"></div>
         <Header/>
         <Router>
             <Routes>
-                <Route path="" view=InitView/>
+                <Route path="/" view=InitView/>
+                <Route path="/about" view=About/>
                 <Route path="/*any" view=|| view! { <h1>"Not Found"</h1> }/>
             </Routes>
         </Router>
