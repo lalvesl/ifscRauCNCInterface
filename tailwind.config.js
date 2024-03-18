9e18a313dc46afd042e17860b8204aadb2d1b3cb
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2024-03-18 11:27:01 -0300

    Adicionado cores do IFSC ao projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
+import colors from 'tailwindcss/colors';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: {
-      files: ["*.html", "./src/**/*.rs"],
+    files: ["*.html", "./src/**/*.rs"],
   },
   darkMode: "class",
   theme: {
-      extend: {},
+    colors: {
+      "gifsc": "#32A041",
+      "rifsc": "#C8191E",
+      ...colors
+    }
   },
-  plugins: [],
+  safelist: [
+    "bg-gifsc",
+    "text-gifsc",
+    "bg-rifsc",
+    "text-rifsc",
+    "hover:bg-gifsc",
+    "hover:text-gifsc",
+    "hover:bg-rifsc",
+    "hover:text-rifsc",
+  ],
+  plugins: [
+    "cssnano"
+  ],
 }
