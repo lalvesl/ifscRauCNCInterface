6e22680721a69e7074df82b975980bd9db06120e
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2024-03-18 11:39:42 -0300

    Simplificado tela inicial

--- src/views/init.rs
+++ src/views/init.rs
@@ -1,12 +1,15 @@
+use crate::components::button::Button;
 use leptos::*;
 
 #[component]
 pub fn InitView() -> impl IntoView {
     view! {
-        <main class="init-main">
-            <h1>Fresadora CNC para PCIs</h1>
-            <h2>Insira um pendrive</h2>
-        </main>
-        <h3 id="edital">Edital nº 02/2023/PROPPI - Universal</h3>
+        <div class="text-gifsc text-center h-full flex flex-col items-center justify-center">
+            <h1 class="my-2 text-5xl font-bold">Fresadora CNC para PCIs</h1>
+            <h2 class="my-2 text-3xl font-semibold">Insira um pendrive</h2>
+        </div>
+        <a href="/about" class="absolute bottom-0 right-0 m-4">
+            <Button text="Ajuda & Sobre".to_string()/>
+        </a>
     }
 }
