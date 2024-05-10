91878d0c89e043186f6adb1b13ecd937b11066ff
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2024-05-10 11:45:02 -0300

    feat: add more tones of green colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import colors from 'tailwindcss/colors';
+import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -8,10 +8,18 @@ module.exports = {
   darkMode: "class",
   theme: {
     colors: {
-      "gifsc": "#32A041",
-      "rifsc": "#C8191E",
-      ...colors
-    }
+      gifsc: "#32A041",
+      rifsc: "#C8191E",
+      ifsc: {
+        g: {
+          1: "#23702D", //0.7
+          2: "#288034", //0.8
+          3: "#2A8537", //0.85
+          4: "#32A041", //1
+        },
+      },
+      ...colors,
+    },
   },
   safelist: [
     "bg-gifsc",
@@ -23,7 +31,5 @@ module.exports = {
     "hover:bg-rifsc",
     "hover:text-rifsc",
   ],
-  plugins: [
-    "cssnano"
-  ],
-}
+  plugins: ["cssnano"],
+};
